73e563bcde1b13e8c6595dc1c1a1ffd8b55b8704
Neville Tummon <nt.devs@gmail.com>
2017-03-01 08:38:44 +1100

    linting fixes and comments for self

--- hash-traverser.py
+++ hash-traverser.py
@@ -1,12 +1,17 @@
 
-import fnmatch, hmac, hashlib, sys, os
+import fnmatch
+import hashlib
+import hmac
+import os
+import sys
 
-def hmac_sha(filename, key = ""):
-    f = open(filename, 'rb')
+# Limited to small files. Currently cannot handle large files
+def hmac_sha(filename, key=""):
+    file = open(filename, 'rb')
     try:
-        body = f.read()
+        body = file.read()
     finally:
-        f.close()
+        file.close()
 
     seed = key.encode('utf-8')
     digest = hmac.new(seed, body, hashlib.sha1).hexdigest()
@@ -22,7 +27,7 @@ def find_files(directory, pattern):
 def main():
     if (len(sys.argv) < 2):
         print("Usage: {0} filepath".format(sys.argv[0]))
-        return
+        sys.exit(1)
 
     for filepath in find_files(sys.argv[1], '*'):
         print("{0}, {1}".format(hmac_sha(filepath), filepath))
